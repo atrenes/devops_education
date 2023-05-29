b3a7cee7a6442a969e2b61ed27b5f7f5f1fe94e0
Danil Khanalainen <atrenes@tune-it.ru>
2023-05-29 21:09:54 +0300

    add priority for replicaSet mode

--- scripts/rs-init.sh
+++ scripts/rs-init.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-DELAY=5
+DELAY=15
 
 mongo <<EOF
-rs.initiate();
+var config = {
+    "_id": "dbrs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "mongo_source:27017",
+            "priority": 2
+        },
+        {
+            "_id": 2,
+            "host": "mongo_target:27017",
+            "priority": 1
+        }
+    ]
+};
+rs.initiate(config, { force: true });
 EOF
 
 echo "------ Waiting for ${DELAY} seconds for mongo_source configuration to be applied ------"
