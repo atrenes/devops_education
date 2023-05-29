357a5cad0ac7ffcb4239f6dc63491dc6ec60ffcb
Danil Khanalainen <atrenes@tune-it.ru>
2023-05-29 18:59:34 +0300

    insertMany instead of insert

--- mongo-init.js
+++ mongo-init.js
@@ -1,29 +1,28 @@
-db.createUser(
-    {
-        user: "mongouser",
-        pwd: "mongouser",
-        roles: [
-            {
-                role: "readWrite",
-                db: "test"
-            }
-        ]
-    }
-);
+rs.status();
+db.createUser({user: 'admin', pwd: 'admin', roles: [ { role: 'root', db: 'admin' } ]});
 db.createCollection('users');
-db.users.insert(
-    {
-        "_id": 1,
-        "firstname": "Angel",
-        "lastname": "DeadInside",
-        "age": 20,
-        "email": "angel@mail.ru"
-    },
-    {
-        "_id": 2,
-        "firstname": "Demon",
-        "lastname": "Marmeladov",
-        "age": 20,
-        "email": "demon@mail.ru"
-    }
+db.users.insertMany(
+    [
+        {
+            "_id": 1,
+            "firstname": "Angel",
+            "lastname": "DeadInside",
+            "age": 20,
+            "email": "angel@mail.ru"
+        },
+        {
+            "_id": 2,
+            "firstname": "Demon",
+            "lastname": "Marmeladov",
+            "age": 20,
+            "email": "demon@mail.ru"
+        },
+        {
+            "_id": 3,
+            "firstname": "Joe",
+            "lastname": "Pencilson",
+            "age": 66,
+            "email": "jp@mail.ru"
+        }
+    ]
 );
