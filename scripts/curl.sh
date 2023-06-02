395c5223d171f8d71f640fe2673a20f6baeaba6e
Danil Khanalainen <atrenes@tune-it.ru>
2023-06-02 04:45:36 +0300

    finished

--- scripts/curl.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @data.json
--- start.sh
+++ start.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 DELAY=5
-
-#docker-compose --file docker-compose.yml down
-#docker rm -f $(docker ps -a -q)
-#docker volume rm $(docker volume ls -q)
+WAIT_KAFKA=45
+docker-compose --file docker-compose.yml down
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
 
 docker-compose --file docker-compose.yml up -d
 
-echo "------ Waiting for ${DELAY} seconds for containers to go up ------"
+echo "------ Waiting for ${DELAY} seconds for mongo to start ------"
 sleep $DELAY
 echo "------ Running mongo_source initiation ------"
 docker exec mongo_source ./scripts/rs-init.sh
 echo "------ mongo_source initiation done ------"
+
+echo "------ waiting for ${WAIT_KAFKA} seconds for kafka to start ------"
+sleep $WAIT_KAFKA
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @./scripts/mongo_source_config.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @./scripts/mongo_target_config.json
+echo "\n------ good to go! ------"
